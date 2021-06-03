795cb6f3c008fa92c6c2b75d6eab06e7ca854c0d
Артем Нанавов <nanavov_aa@km-union.ru>
2021-06-03 22:29:45 +0300

    refactoring

--- webpack/static-server.js
+++ webpack/static-server.js
@@ -11,7 +11,7 @@ const rootPath = path.join(__dirname, '../');
 
 app.use();
 
-app.use('/app', express.static(`${rootPath}dist`));
+app.use('/', express.static(`${rootPath}dist`));
 
 app.get('*', (_, res) => {
 	res.sendFile(`${rootPath}dist/index.html`);
